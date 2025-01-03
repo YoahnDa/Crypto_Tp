dafb50ad7325f0b01d7a28e4985aa34456f5c3cf
YoahnDa <143202366+YoahnDa@users.noreply.github.com>
2025-01-03 21:26:20 +0300

    second commit
    CRUD crypto

---
+++ Backend_Crypto/Helper/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Backend_Crypto.Dto;
+using Backend_Crypto.Models;
+
+namespace Backend_Crypto.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles() 
+        {
+            CreateMap<Crypto,CryptoDto>();
+            CreateMap<HistoriquePrix,HistoriqueDto>();
+            CreateMap<CryptoUpdateDto, Crypto>();
+        }
+    }
+}
--- Backend_Crypto/Program.cs
+++ Backend_Crypto/Program.cs
@@ -1,8 +1,19 @@
+using Backend_Crypto.Data;
+using Backend_Crypto.Interfaces;
+using Backend_Crypto.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection"));
+});
+builder.Services.AddScoped<ICryptoRepository, CryptoRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
